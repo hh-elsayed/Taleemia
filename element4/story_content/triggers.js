537f22303ef7b1503bfaa2f69dc7a8065fc97f60
Hesham Hussein El-Sayed <hhs.elsayed@gmail.com>
2026-02-02 17:07:13 +0200

    Add files via upload

---
+++ element4/story_content/triggers.js
@@ -0,0 +1,113 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6Uu8sJtXIDK":
+        Script1();
+        break;
+      case "6d29BA4iiXk":
+        Script2();
+        break;
+      case "60e1wKTopw5":
+        Script3();
+        break;
+      case "6orl9y8byA8":
+        Script4();
+        break;
+      case "6B5Zi8Emt6i":
+        Script5();
+        break;
+      case "5W1WcFAuXtA":
+        Script6();
+        break;
+      case "5rbRq5VnKCD":
+        Script7();
+        break;
+      case "6IcEDoERzHt":
+        Script8();
+        break;
+      case "5a7HlHxaQsf":
+        Script9();
+        break;
+      case "5yRM87niIUS":
+        Script10();
+        break;
+      case "64egKPgTfDD":
+        Script11();
+        break;
+      case "5qrVXuJCmBV":
+        Script12();
+        break;
+      case "6kpULQ88heC":
+        Script13();
+        break;
+      case "6gbtHcUm2cq":
+        Script14();
+        break;
+      case "5d9sMbQQ9Ol":
+        Script15();
+        break;
+      case "6PWlu9OMyq1":
+        Script16();
+        break;
+      case "6Ur8YccZWtr":
+        Script17();
+        break;
+      case "67qAJEbEVSd":
+        Script18();
+        break;
+      case "66345GlkiKk":
+        Script19();
+        break;
+      case "5lID1JET74X":
+        Script20();
+        break;
+      case "5c7yuOEXFvN":
+        Script21();
+        break;
+      case "68wQJom0kqI":
+        Script22();
+        break;
+      case "5lUwgGji4Ri":
+        Script23();
+        break;
+      case "64UenKxCk6F":
+        Script24();
+        break;
+      case "5bfgXwVhTJv":
+        Script25();
+        break;
+      case "5fshWdPmxbh":
+        Script26();
+        break;
+      case "6qsFhdCQOYn":
+        Script27();
+        break;
+      case "6V0WgKcKaWO":
+        Script28();
+        break;
+      case "6Yi10SSYFfE":
+        Script29();
+        break;
+      case "6p2AZaM43vJ":
+        Script30();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+};
