503f44d2f274cf44d804702a681779b6c7bdb93f
Hesham Hussein El-Sayed <hhs.elsayed@gmail.com>
2026-02-02 15:00:48 +0200

    Add files via upload

---
+++ element1/story_content/triggers.js
@@ -0,0 +1,35 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5zrKUiHK914":
+        Script1();
+        break;
+      case "6b0sEjkKdCW":
+        Script2();
+        break;
+      case "5dEHcCvhCPF":
+        Script3();
+        break;
+      case "5iNaHkhKtsU":
+        Script4();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+};
