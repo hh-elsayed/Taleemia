28617b176f24b2344f97f2a77e3b2cd41afab99b
Hesham Hussein El-Sayed <hhs.elsayed@gmail.com>
2026-02-03 17:50:03 +0200

    Add files via upload

---
+++ element24/story_content/triggers.js
@@ -0,0 +1,77 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "64aGCvisF0f":
+        Script1();
+        break;
+      case "6VMrBm0oF7e":
+        Script2();
+        break;
+      case "6QrOkblmBOP":
+        Script3();
+        break;
+      case "6eS8isLQF16":
+        Script4();
+        break;
+      case "6JTa8gqnfxy":
+        Script5();
+        break;
+      case "66fiuwdrHR5":
+        Script6();
+        break;
+      case "6BCUo4DUY1p":
+        Script7();
+        break;
+      case "5jBy1Sy9N8R":
+        Script8();
+        break;
+      case "6Hg2V2Hs4bz":
+        Script9();
+        break;
+      case "6TYZerMiIZL":
+        Script10();
+        break;
+      case "6XJhT9O4Hif":
+        Script11();
+        break;
+      case "5f4Khdpo8Fo":
+        Script12();
+        break;
+      case "5w30qsYo70W":
+        Script13();
+        break;
+      case "5afOzTHqv7a":
+        Script14();
+        break;
+      case "6pJTadsWfFA":
+        Script15();
+        break;
+      case "6gjQuw9LatX":
+        Script16();
+        break;
+      case "6IBsicmLmtM":
+        Script17();
+        break;
+      case "5vxuLMKK8Vo":
+        Script18();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+};
---
+++ element24/story_content/user.js
@@ -0,0 +1,106 @@
+window.InitUserScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+window.Script1 = function()
+{
+  DS.appState.onToggleVolume(0);
+}
+
+window.Script2 = function()
+{
+  DS.appState.onToggleVolume(1);
+}
+
+window.Script3 = function()
+{
+  DS.appState.onToggleVolume(0);
+}
+
+window.Script4 = function()
+{
+  DS.appState.onToggleVolume(1);
+}
+
+window.Script5 = function()
+{
+  DS.appState.onToggleVolume(0);
+}
+
+window.Script6 = function()
+{
+  DS.appState.onToggleVolume(1);
+}
+
+window.Script7 = function()
+{
+  DS.appState.onToggleVolume(0);
+}
+
+window.Script8 = function()
+{
+  DS.appState.onToggleVolume(1);
+}
+
+window.Script9 = function()
+{
+  DS.appState.onToggleVolume(0);
+}
+
+window.Script10 = function()
+{
+  DS.appState.onToggleVolume(1);
+}
+
+window.Script11 = function()
+{
+  DS.appState.onToggleVolume(0);
+}
+
+window.Script12 = function()
+{
+  DS.appState.onToggleVolume(1);
+}
+
+window.Script13 = function()
+{
+  DS.appState.onToggleVolume(0);
+}
+
+window.Script14 = function()
+{
+  DS.appState.onToggleVolume(1);
+}
+
+window.Script15 = function()
+{
+  DS.appState.onToggleVolume(0);
+}
+
+window.Script16 = function()
+{
+  DS.appState.onToggleVolume(1);
+}
+
+window.Script17 = function()
+{
+  DS.appState.onToggleVolume(0);
+}
+
+window.Script18 = function()
+{
+  DS.appState.onToggleVolume(1);
+}
+
+};
