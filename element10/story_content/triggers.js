00b58f8483c9386cc4391eddcef8a0c8cfc2a211
Hesham Hussein El-Sayed <hhs.elsayed@gmail.com>
2026-02-03 17:34:16 +0200

    Add files via upload

---
+++ element10/story_content/triggers.js
@@ -0,0 +1,71 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6G8OYuHipqy":
+        Script1();
+        break;
+      case "6ThxNdkGPuJ":
+        Script2();
+        break;
+      case "61YdGfa7Hk9":
+        Script3();
+        break;
+      case "6O0wzImj3w0":
+        Script4();
+        break;
+      case "6fIN6ovOqES":
+        Script5();
+        break;
+      case "6fBedWqUMM4":
+        Script6();
+        break;
+      case "5rnvRMokMjY":
+        Script7();
+        break;
+      case "5YMBMox2nPM":
+        Script8();
+        break;
+      case "5aLY6yvGvTw":
+        Script9();
+        break;
+      case "5UjIexo1qXi":
+        Script10();
+        break;
+      case "6jFoKnBA5WY":
+        Script11();
+        break;
+      case "5w3iFciwFDW":
+        Script12();
+        break;
+      case "6lWuNLA0sHq":
+        Script13();
+        break;
+      case "5bjWl2Il8JY":
+        Script14();
+        break;
+      case "6dBs1ytzJ1u":
+        Script15();
+        break;
+      case "6oDFU31SYkz":
+        Script16();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+};
