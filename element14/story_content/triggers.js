426e4cf57f8c623caddee1602ae25a710710531c
Hesham Hussein El-Sayed <hhs.elsayed@gmail.com>
2026-02-03 17:36:36 +0200

    Add files via upload

---
+++ element14/story_content/triggers.js
@@ -0,0 +1,59 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6ibuUt89dK1":
+        Script1();
+        break;
+      case "6n7u9QH1mhc":
+        Script2();
+        break;
+      case "5py2f6eNyhQ":
+        Script3();
+        break;
+      case "69fDeLFZLqD":
+        Script4();
+        break;
+      case "6PJKEH4ig8E":
+        Script5();
+        break;
+      case "5cR46Z8YNEl":
+        Script6();
+        break;
+      case "5xPFRdXQl5r":
+        Script7();
+        break;
+      case "6NemvnFFtbJ":
+        Script8();
+        break;
+      case "6BGFqt59zxn":
+        Script9();
+        break;
+      case "5nyhaHnaFN2":
+        Script10();
+        break;
+      case "6mtgZfQAIvm":
+        Script11();
+        break;
+      case "6msSwK5YauG":
+        Script12();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+};
