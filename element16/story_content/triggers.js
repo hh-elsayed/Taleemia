c92513815b0be45bbff6f132829ff886373eccb2
Hesham Hussein El-Sayed <hhs.elsayed@gmail.com>
2026-02-03 17:43:28 +0200

    Add files via upload

---
+++ element16/story_content/triggers.js
@@ -0,0 +1,137 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6ZXyk7h4fKy":
+        Script1();
+        break;
+      case "5zST34UhchK":
+        Script2();
+        break;
+      case "6hgddTYv5fL":
+        Script3();
+        break;
+      case "5yQHy9aaNVt":
+        Script4();
+        break;
+      case "6IreKUtyrLK":
+        Script5();
+        break;
+      case "5zvZRHSFy25":
+        Script6();
+        break;
+      case "5knaw7i19gM":
+        Script7();
+        break;
+      case "5eO35y5Uf0T":
+        Script8();
+        break;
+      case "6pfAmE1M3Hw":
+        Script9();
+        break;
+      case "5pCuhH5mIqz":
+        Script10();
+        break;
+      case "6eMYHLnO7t9":
+        Script11();
+        break;
+      case "6lPrkljcccM":
+        Script12();
+        break;
+      case "6mgqgeyl7gw":
+        Script13();
+        break;
+      case "5YLpc6eCnOD":
+        Script14();
+        break;
+      case "6AKl83zAdmG":
+        Script15();
+        break;
+      case "5ntz7iwBRMe":
+        Script16();
+        break;
+      case "67BtHtHtdZA":
+        Script17();
+        break;
+      case "6cj0ooljInM":
+        Script18();
+        break;
+      case "6ksFmGm7OY6":
+        Script19();
+        break;
+      case "6Jrzhv3cz5C":
+        Script20();
+        break;
+      case "5vtBPGqpEKn":
+        Script21();
+        break;
+      case "6AfzIGwi2Ns":
+        Script22();
+        break;
+      case "6eOry4S78uK":
+        Script23();
+        break;
+      case "6S43lAoI57r":
+        Script24();
+        break;
+      case "6kE5jxOlwBY":
+        Script25();
+        break;
+      case "6CeaFGWZjDz":
+        Script26();
+        break;
+      case "6dICclcvE5u":
+        Script27();
+        break;
+      case "6AVU8LA6xwN":
+        Script28();
+        break;
+      case "5bhsvQgYCkQ":
+        Script29();
+        break;
+      case "5XOcpw2XP5X":
+        Script30();
+        break;
+      case "5ZM3KpQiZiO":
+        Script31();
+        break;
+      case "6TJ4dYjAUTf":
+        Script32();
+        break;
+      case "5gOqdHh1YEQ":
+        Script33();
+        break;
+      case "62rJ9Hw5oX8":
+        Script34();
+        break;
+      case "5aRI3AXC0Tt":
+        Script35();
+        break;
+      case "6IeFVAlNrow":
+        Script36();
+        break;
+      case "6Whgr3Pzw0z":
+        Script37();
+        break;
+      case "5yWT4EncmZS":
+        Script38();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+};
---
+++ element16/story_content/user.js
@@ -0,0 +1,206 @@
+window.InitUserScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+window.Script1 = function()
+{
+  DS.appState.onToggleVolume(0);
+}
+
+window.Script2 = function()
+{
+  DS.appState.onToggleVolume(1);
+}
+
+window.Script3 = function()
+{
+  DS.appState.onToggleVolume(0);
+}
+
+window.Script4 = function()
+{
+  DS.appState.onToggleVolume(1);
+}
+
+window.Script5 = function()
+{
+  DS.appState.onToggleVolume(0);
+}
+
+window.Script6 = function()
+{
+  DS.appState.onToggleVolume(1);
+}
+
+window.Script7 = function()
+{
+  DS.appState.onToggleVolume(0);
+}
+
+window.Script8 = function()
+{
+  DS.appState.onToggleVolume(1);
+}
+
+window.Script9 = function()
+{
+  DS.appState.onToggleVolume(0);
+}
+
+window.Script10 = function()
+{
+  DS.appState.onToggleVolume(1);
+}
+
+window.Script11 = function()
+{
+  DS.appState.onToggleVolume(0);
+}
+
+window.Script12 = function()
+{
+  DS.appState.onToggleVolume(1);
+}
+
+window.Script13 = function()
+{
+  DS.appState.onToggleVolume(0);
+}
+
+window.Script14 = function()
+{
+  DS.appState.onToggleVolume(1);
+}
+
+window.Script15 = function()
+{
+  DS.appState.onToggleVolume(0);
+}
+
+window.Script16 = function()
+{
+  DS.appState.onToggleVolume(1);
+}
+
+window.Script17 = function()
+{
+  DS.appState.onToggleVolume(0);
+}
+
+window.Script18 = function()
+{
+  DS.appState.onToggleVolume(1);
+}
+
+window.Script19 = function()
+{
+  DS.appState.onToggleVolume(0);
+}
+
+window.Script20 = function()
+{
+  DS.appState.onToggleVolume(1);
+}
+
+window.Script21 = function()
+{
+  DS.appState.onToggleVolume(0);
+}
+
+window.Script22 = function()
+{
+  DS.appState.onToggleVolume(1);
+}
+
+window.Script23 = function()
+{
+  DS.appState.onToggleVolume(0);
+}
+
+window.Script24 = function()
+{
+  DS.appState.onToggleVolume(1);
+}
+
+window.Script25 = function()
+{
+  DS.appState.onToggleVolume(0);
+}
+
+window.Script26 = function()
+{
+  DS.appState.onToggleVolume(1);
+}
+
+window.Script27 = function()
+{
+  DS.appState.onToggleVolume(0);
+}
+
+window.Script28 = function()
+{
+  DS.appState.onToggleVolume(1);
+}
+
+window.Script29 = function()
+{
+  DS.appState.onToggleVolume(0);
+}
+
+window.Script30 = function()
+{
+  DS.appState.onToggleVolume(1);
+}
+
+window.Script31 = function()
+{
+  DS.appState.onToggleVolume(0);
+}
+
+window.Script32 = function()
+{
+  DS.appState.onToggleVolume(1);
+}
+
+window.Script33 = function()
+{
+  DS.appState.onToggleVolume(0);
+}
+
+window.Script34 = function()
+{
+  DS.appState.onToggleVolume(1);
+}
+
+window.Script35 = function()
+{
+  DS.appState.onToggleVolume(0);
+}
+
+window.Script36 = function()
+{
+  DS.appState.onToggleVolume(1);
+}
+
+window.Script37 = function()
+{
+  DS.appState.onToggleVolume(0);
+}
+
+window.Script38 = function()
+{
+  DS.appState.onToggleVolume(1);
+}
+
+};
